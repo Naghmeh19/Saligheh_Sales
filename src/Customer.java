54ba1f4226cc6c47e8aa5051a9e86ed9660f766a
naghmeh19 <sal19867@spengergasse.at>
2020-04-09 18:05:30 +0200

    Create the class Customer

---
+++ src/Customer.java
@@ -0,0 +1,25 @@
+
+public class Customer {
+	 int customerID;
+	 String fisrtName;
+	 
+	public Customer(int customerID, String fisrtName) {
+		super();
+		this.customerID = customerID;
+		this.fisrtName = fisrtName;
+	}
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getFisrtName() {
+		return fisrtName;
+	}
+	public void setFisrtName(String fisrtName) {
+		this.fisrtName = fisrtName;
+	}
+	 
+
+}
